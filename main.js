53e5c8905c30071959cdb85497fe57b1d8b77902
Samuel Montessi do Amaral Silva <samuel.montessi@estudante.ufjf.br>
2025-07-20 22:03:27 -0300

    adiciona função atualizarLista

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 //main.js
 
 const olItens = document.querySelector("#itens");
-const pEntrada = document.querySelector('#entrada');
+const pEntrada = document.querySelector("#entrada");
 const btnAdicionar = document.querySelector("#adicionar");
 const btnLimpar = document.querySelector("#limpar");
+
+atualizarLista();
+
+function atualizarLista() {
+  olItems.innerHTML = "";
+  let lista = getLista();
+  for (let i = 0; i < lista.length; i++) {
+    const li = document.createElement("li");
+    li.textContent = lista[i];
+    olItems.appendChild(li);
+  }
+}
